5b5c1d7cbcd7e2e00d34e31e410c53eee6821186
Michael A. Plikk <michael@aptoma.com>
2018-12-25 23:59:28 +0100

    Solved day 7, part 2

--- 2018/07/src/main.rs
+++ 2018/07/src/main.rs
@@ -2,7 +2,6 @@
 extern crate lazy_static;
 
 use std::collections::{HashMap};
-use std::str::FromStr;
 use std::io::{self, prelude::*};
 
 use regex::Regex;
@@ -22,16 +21,98 @@ fn main() -> Result<(), Error> {
         steps.entry(req).or_insert(Step::new(req)).children.push(step);
     }
 
-    let mut order = String::new();
+    let mut time = 0;
+    let mut workers = vec![Worker {
+        id: 1,
+        ready_at: 0,
+        working_on: None
+    }, Worker {
+        id: 2,
+        ready_at: 0,
+        working_on: None
+    }, Worker {
+        id: 3,
+        ready_at: 0,
+        working_on: None
+    }, Worker {
+        id: 4,
+        ready_at: 0,
+        working_on: None
+    }, Worker {
+        id: 5,
+        ready_at: 0,
+        working_on: None
+    }];
+    let mut done = vec![];
     while !steps.is_empty() {
-        let ready = get_next_step(&steps);
-        perform_step(ready.unwrap(), &mut steps);
-        order.push(ready.unwrap());
+        println!("Time: {}", time);
+        let mut done_something = false;
+        match get_available_step(&mut steps) {
+            Some(available_step) => {
+                match get_available_worker(&mut workers) {
+                    Some(mut worker) => {
+                        println!("Time: {}, Worker: {:?}", time, worker);
+                        println!("Starting work on: {}", available_step.name);
+                        worker.working_on = Some(available_step.name);
+                        worker.ready_at =  time + available_step.time();
+                        workers.push(worker);
+                        done_something = true;
+                    },
+                    None => {
+                        steps.insert(available_step.name, available_step);
+                    }
+                }
+            },
+            None => {
+            }
+        }
+
+        if !done_something {
+            if let Some((new_time, step)) = perform_work(&mut workers) {
+                done.push(step);
+                perform_step(step, &mut steps);
+                time = new_time;
+            }
+        }
     }
-    println!("Order: {}", order); // FMOXCDGJRAUIHKNYZTESWLPBQV
+    while let Some((new_time, step)) = perform_work(&mut workers) {
+        done.push(step);
+        time = new_time;
+    } 
+
+    println!("Steps: {:?}", done);
+    println!("Time: {}", time);
 
 
     Ok(())
+}
+
+fn perform_work(workers: &mut Vec<Worker>) -> Option<(i32, char)> {
+    workers.sort_by_key(|w| w.ready_at);
+    println!("Workers: {:?}", workers);
+    let index = workers.iter().position(|w| w.working_on.is_some())?;
+    let mut worker = workers.remove(index);
+    if worker.working_on.is_some() {
+        println!("Performed work: {:?}", worker);
+        let work = (worker.ready_at, worker.working_on.expect("Worker has no work to perform!"));
+        worker.working_on = None;
+        workers.push(worker);
+        Some(work)
+    } else {
+        println!("no work to perform");
+        workers.push(worker);
+        None
+    }
+}
+
+fn get_available_step(steps: &mut HashMap<char, Step>) -> Option<Step> {
+    let (&c, _) = steps.iter().find(|(_, s)| s.parents.is_empty())?;
+    steps.remove(&c)
+}
+
+fn get_available_worker(workers: &mut Vec<Worker>) -> Option<Worker> {
+    let index = workers.iter().position(|w| w.working_on.is_none())?;
+    Some(workers.remove(index))
 }
 
 fn perform_step(name: char, steps: &mut HashMap<char, Step>) {
@@ -42,24 +123,18 @@ fn perform_step(name: char, steps: &mut HashMap<char, Step>) {
     });
 }
 
-fn get_next_step(steps: &HashMap<char, Step>) -> Option<char> {
-    let mut ready = steps.iter().filter_map(|(_, v)| {
-        if v.parents.is_empty() {
-            Some(v.name)
-        } else {
-            None
-        }
-    }).collect::<Vec<_>>();
-    ready.sort();
-    ready.reverse();
-    ready.pop()
+#[derive(Debug, Clone)]
+struct Worker {
+    id: usize,
+    ready_at: i32,
+    working_on: Option<char>
 }
 
 #[derive(Debug, Clone)]
 struct Step {
     name: char,
     parents: Vec<char>,
-    children: Vec<char>
+    children: Vec<char>,
 }
 
 impl Step {
@@ -69,6 +144,10 @@ impl Step {
             parents: vec![],
             children: vec![]
         }
+    }
+
+    fn time(&self) -> i32 {
+       (self.name as i32) - 64 + 60
     }
 }
 
